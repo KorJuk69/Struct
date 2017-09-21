ef9d8f495643c7593367d3ed1a833f7f8175f25f
KorJuk69 <korjuk1@gmail.com>
2017-09-21 14:13:12 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,32 @@ package com.company;
 
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        double massiv [];
+        massiv = new double[n];
 
+        for (int i=0;i<n;i++){
+
+            System.out.print("x: ");
+            double x = scanner.nextInt();
+            System.out.print("y: ");
+            double y = scanner.nextInt();
+
+           massiv [i] = (x+y)/2;
+
+        }
+
+        for (int i=0;i<n;i++) {
+
+            System.out.print(massiv [i] + " ");
+
+        }
     }
 }
