37d8929acaf004304f78625d834c98fe945d4887
KorJuk69 <korjuk1@gmail.com>
2017-09-21 14:39:18 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,40 @@ package com.company;
 
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
 
+        double massivx [];
+        massivx = new double[n];
+        double massivy [];
+        massivy = new double[n];
+        double maxdiametr = 0;
+
+        for (int i=0;i<n;i++){
+
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            massivx [i] = x;
+            massivy [i] = y;
+
+            if (i>0){
+
+                double dx = massivx [i] -  massivx [i-1];
+                double dy = massivy [i] -  massivy [i-1];
+                double diametr = sqrt(dx*dx+dy*dy);
+
+                if (diametr>maxdiametr) maxdiametr = diametr;
+
+            }
+        }
+
+        System.out.print(maxdiametr);
     }
 }
