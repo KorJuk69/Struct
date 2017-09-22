7f3d522a227f6b77f7295f571090de4206390f8e
KorJuk69 <korjuk1@gmail.com>
2017-09-22 17:14:22 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        double srmat = 0;
+        double srfiz = 0;
+        double srinf = 0;
+
+        for (int i = 0;i<n;i++){
+
+            String Fam = scanner.nextLine();
+            String Im = scanner.nextLine();
+            int mat = scanner.nextInt();
+            int fiz = scanner.nextInt();
+            int inf = scanner.nextInt();
+
+            srmat += mat;
+            srfiz += fiz;
+            srinf += inf;
+
+        }
+
+        System.out.print(srmat/n + " " + srfiz/n + " " + srinf/n);
 
     }
 }
